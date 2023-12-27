72abe67e14be79c6b5c26087fd58662c6c604947
johnmarkbanisilon <zionsploit@gmail.com>
2023-12-27 22:59:24 +0800

    SwiftUI InitialCommits

---
+++ Landmarks/CircleImageView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleImageView.swift
+//  Landmarks
+//
+//  Created by John Mark Banisilon on 12/27/23.
+//
+
+import SwiftUI
+
+struct CircleImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleImageView()
+}
---
+++ Landmarks/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by John Mark Banisilon on 12/27/23.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
